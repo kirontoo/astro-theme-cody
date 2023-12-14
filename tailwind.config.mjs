8c3d8e4ed4758b8e53552400c75e64d0878a13e0
Amy Dang <kirontoo@gmail.com>
2023-12-14 15:35:08 -0800

    add space mono font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
+		fontFamily: {
+			'mono': ['Space Mono', 'monospace']
+		},
 		extend: {
 			colors: {
 				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
