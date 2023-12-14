12e7e681acdae15ce44756ecabf9228ed70a2094
Amy Dang <kirontoo@gmail.com>
2023-12-13 17:39:44 -0800

    configure tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
+				textColor: "hsl(var(--theme-text) / <alpha-value>)",
+				link: "hsl(var(--theme-link) / <alpha-value>)",
+				accent: "hsl(var(--theme-accent) / <alpha-value>)",
+				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
+				surface: "hsl(var(--theme-surface) / <alpha-value>)",
+			},
+		}
 	},
 	plugins: [],
 }
