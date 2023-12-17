de5f4482fe5dba29ba106f7b1aa40017cb33f662
Amy Dang <kirontoo@gmail.com>
2023-12-16 17:00:26 -0800

    add tags to frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
+function removeDupsAndLowercase(list: string[]) {
+	if (!list.length) return list;
+	const lowercaseItems = list.map((str) => str.toLowerCase());
+	const uniqueItems = new Set(lowercaseItems);
+	return Array.from(uniqueItems);
+
+}
+
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
@@ -10,6 +18,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string()).default([]).transform(removeDupsAndLowercase)
 	}),
 });
 
