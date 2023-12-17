319f5f14d2f60e9ae0f2337b57136347e6784890
Amy Dang <kirontoo@gmail.com>
2023-12-16 19:57:01 -0800

    add ogImage frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,6 +18,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		ogImage: z.string().optional(),
 		tags: z.array(z.string()).default([]).transform(removeDupsAndLowercase)
 	}),
 });
