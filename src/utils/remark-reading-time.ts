1b12e58b182b2cedfd2551ba8c0789561dad122c
Amy Dang <kirontoo@gmail.com>
2023-12-18 16:29:27 -0800

    add reading time

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,6 +1,7 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
+// https://docs.astro.build/en/recipes/reading-time/#recipe
 export function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
