1256fcaca39728369e752b2cb195b8657952a67f
Amy Dang <kirontoo@gmail.com>
2024-01-17 12:46:31 -0800

    change site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://astro-theme-cody.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
